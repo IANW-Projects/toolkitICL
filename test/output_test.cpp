e95c0e3ecb736a17d57487b097cbff128fc809d4
Hendrik Ranocha <ranocha@users.noreply.github.com>
2019-02-11 07:53:02 +0100

    add output test (#8)

--- test/output_test.cpp
+++ test/output_test.cpp
@@ -16,7 +16,7 @@ int main(void)
 {
   constexpr int LENGTH = 32;
 
-  string filename{"repetition_test.h5"};
+  string filename{"output_test.h5"};
 
   if (fileExists(filename)) {
     remove(filename.c_str());
@@ -45,7 +45,7 @@ kernel void add_one(global REAL* values)\n\
   h5_write_string(filename.c_str(), "Kernel_URL", kernel_url.c_str());
   vector<string> kernels(1, string("add_one"));
   h5_write_strings(filename.c_str(), "Kernels", kernels);
-  cl_ulong kernel_repetitions = 5;
+  cl_ulong kernel_repetitions = 10000;
   h5_write_single<cl_ulong>(filename.c_str(), "Kernel_Repetitions", kernel_repetitions);
 
   // ranges
@@ -77,7 +77,7 @@ kernel void add_one(global REAL* values)\n\
   }
 
 
-  // check result
+  // check whether all necessary output is present
   string out_filename("out_");
   out_filename.append(filename);
   vector<cl_ulong> values_test(LENGTH);
@@ -87,13 +87,55 @@ kernel void add_one(global REAL* values)\n\
     return 1;
   }
 
-  h5_read_buffer<cl_ulong>(out_filename.c_str(), "Data/values", &values_test[0]);
-  for (size_t idx = 0; idx < LENGTH; ++idx) {
-    if (values_test[idx] != values[idx] + kernel_repetitions) {
-      cerr << "Error: Result 'values[" << idx << "] == " << values_test[idx] << "' is not as expected [" << values[idx] + kernel_repetitions << "]." << endl;
-      return 1;
-    }
-  }
+  double Data_LoadTime = h5_read_single<double>(out_filename.c_str(), "Data_LoadTime");
+  cout << "Data_LoadTime   = " << Data_LoadTime << endl;
+
+  double Data_StoreTime = h5_read_single<double>(out_filename.c_str(), "Data_StoreTime");
+  cout << "Data_StoreTime  = " << Data_StoreTime << endl;
+
+  double Kernel_ExecTime = h5_read_single<double>(out_filename.c_str(), "Kernel_ExecTime");
+  cout << "Kernel_ExecTime = " << Kernel_ExecTime << endl;
+
+  double Total_ExecTime = h5_read_single<double>(out_filename.c_str(), "Total_ExecTime");
+  cout << "Total_ExecTime  = " << Total_ExecTime << endl;
+
+  string Kernel_ExecStart;
+  h5_read_string(out_filename.c_str(), "Kernel_ExecStart", Kernel_ExecStart);
+  cout << "Kernel_ExecStart = " << Kernel_ExecStart << endl;
+
+  string Kernel_Settings;
+  h5_read_string(out_filename.c_str(), "Kernel_Settings", Kernel_Settings);
+  cout << "Kernel_Settings  = " << Kernel_Settings << endl;
+
+  cl_int Global_Range[3];
+  h5_read_buffer<cl_int>(out_filename.c_str(), "Global_Range", &Global_Range[0]);
+  cout << "Global_Range = (" << Global_Range[0]
+                     << ", " << Global_Range[1]
+                     << ", " << Global_Range[2] << ")" << endl;
+
+  cl_int Local_Range[3];
+  h5_read_buffer<cl_int>(out_filename.c_str(), "Local_Range", &Local_Range[0]);
+  cout << "Local_Range  = (" << Local_Range[0]
+                     << ", " << Local_Range[1]
+                     << ", " << Local_Range[2] << ")" << endl;
+
+  cl_int Range_Start[3];
+  h5_read_buffer<cl_int>(out_filename.c_str(), "Range_Start", &Range_Start[0]);
+  cout << "Range_Start  = (" << Range_Start[0]
+                     << ", " << Range_Start[1]
+                     << ", " << Range_Start[2] << ")" << endl;
+
+  string Host_OS;
+  h5_read_string(out_filename.c_str(), "Host_OS", Host_OS);
+  cout << "Host_OS = " << Host_OS << endl;
+
+  string OpenCL_Device;
+  h5_read_string(out_filename.c_str(), "OpenCL_Device", OpenCL_Device);
+  cout << "OpenCL_Device  = " << OpenCL_Device << endl;
+
+  string OpenCL_Version;
+  h5_read_string(out_filename.c_str(), "OpenCL_Version", OpenCL_Version);
+  cout << "OpenCL_Version = " << OpenCL_Version << endl;
 
   //TODO: possible cleanup?
   // if (fileExists(kernel_url)) {
